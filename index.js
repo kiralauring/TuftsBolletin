c54c46d204c77bdb5aa6720e21d00918d7c8e866
Kira Lauring <kiralauring@Kiras-MacBook-Air.local>
2016-10-15 04:31:27 -0400

    we made it boo back end done

--- index.js
+++ index.js
@@ -15,22 +15,46 @@ day = tomorrowDate.getDate();
 month = tomorrowDate.getMonth() + 1;
 year = tomorrowDate.getFullYear();
 var tomorrow = year + "-" + month + "-" + day;
-var events = [];
+var todEvents = [];
+var tomEvents = [];
+var sortTodEvents = [];
+var sortTomEvents = [];
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.get("/",function(request,response){
         for(var i = 0; i < orgs.length; i++){
-                //console.log(orgs[i]);
                 needle.get("https://graph.facebook.com/v2.8/" + orgs[i] + "/events?access_token=1811986875725516%7C5795c6703e4db096806ef20f81de9fd0&__mref=message_bubble", function(error, response) {
                         if (!error && response.statusCode == 200)
                                 for(var j = 0; j < response.body.data.length; j++){
                                         var start = response.body.data[j].start_time;
-                                        if(start.includes(today)||start.includes(tomorrow)){
-                                                console.log(start);
-                                                console.log(response.body.data[j]);
-                                                events.push(response.body.data[j]);
+                                        if(start.includes(today)){
+                                                todEvents.push(response.body.data[j]);
+                                        }
+                                        else if(start.includes(tomorrow)){
+                                                tomEvents.push(response.body.data[j]);
                                         }
                                 }
+                        for(var k = 0; k < todEvents.length; k++){
+                                var eventTime = todEvents[k].start_time.substring(11,13) + todEvents[k].start_time.substring(14,16);
+                                var eventTimeTemp = parseInt(eventTime);
+                                //console.log(eventTimeTemp);
+                                var eventObj = {"time": eventTimeTemp,"event": todEvents[k]}
+                                sortTodEvents[k] = eventObj;
+                                sortTodEvents.sort(function(a, b) {
+                                        return a.time - b.time;
+                                });
+                        }
+
+                        for(var k = 0; k < tomEvents.length; k++){
+                                var eventTime = tomEvents[k].start_time.substring(11,13) + tomEvents[k].start_time.substring(14,16);
+                                var eventTimeTemp = parseInt(eventTime);
+                                //console.log(eventTimeTemp);
+                                var eventObj = {"time": eventTimeTemp,"event": tomEvents[k]}
+                                sortTomEvents[k] = eventObj;
+                                sortTomEvents.sort(function(a, b) {
+                                        return a.time - b.time;
+                                });
+                        }
                 });
         }
 })
