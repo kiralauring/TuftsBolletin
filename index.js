38da1b00e6d54ea4076e8593b59bedf17544caee
Kira Lauring <kiralauring@Kiras-MacBook-Air.local>
2016-10-15 00:48:52 -0400

    got sum data from dat fb

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+var needle = require("needle");
 var app = express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.get("/",function(request,response){
-        console.log("Hello World!");
-        response.send("Heyo");
+        needle.get('https://graph.facebook.com/v2.8/118585304884933/events?access_token=1811986875725516%7C5795c6703e4db096806ef20f81de9fd0&__mref=message_bubble', function(error, response) {
+                if (!error && response.statusCode == 200)
+                        console.log(response.body);
+        });
 })
 app.post("/derp",function(request,response){
         console.log(request.body);
